12ca2554147bd22bbae04ba00f0e887c74e80461
Paul Beckwith <303dog@gmail.com>
2019-10-22 23:13:48 -0700

    bug fix

--- top_100gem/config/environment.rb
+++ top_100gem/config/environment.rb
@@ -3,5 +3,5 @@ require 'nokogiri'
 require 'open-uri'
 
 require './lib/schedule/scraper.rb'
-require './lib/schedule/class_Cli'
-require './lib/schedule/version'
+require './lib/schedule/class_cli.rb'
+require './lib/schedule/version.rb'
